e778030d61e5a9670b02921e1ba6ad79f460651c
dm3 <deadmoro3@gmail.com>
2010-09-06 22:26:14 +0300

    adding fields with generic type parameters in javassist sucks

--- jquickcheck-test/src/main/java/lt/dm3/jquickcheck/test/TestClassBuilder.java
+++ jquickcheck-test/src/main/java/lt/dm3/jquickcheck/test/TestClassBuilder.java
@@ -3,23 +3,88 @@ package lt.dm3.jquickcheck.test;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtField;
+import javassist.Modifier;
+import javassist.NotFoundException;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.ClassFile;
 import javassist.bytecode.ConstPool;
+import javassist.bytecode.Descriptor;
+import javassist.bytecode.FieldInfo;
+import javassist.bytecode.SignatureAttribute;
 import javassist.bytecode.annotation.Annotation;
 import javassist.bytecode.annotation.ClassMemberValue;
 import lt.dm3.jquickcheck.junit4.QuickCheckRunner;
 
 import org.junit.runner.RunWith;
 
-public class TestClassBuilder {
+/**
+ * A builder which builds test classes containing generators. Built test classes should later be used in tests for
+ * different test provider/quickcheck backend combinations. The main idea is to supply the builder with the name of a
+ * class, the test engine and the set of generators to be placed inside of the test.
+ * 
+ * @author dm3
+ * 
+ * @param <T>
+ */
+public class TestClassBuilder<T> {
 
-    public void build(String name) {
+    public enum AccessModifier {
+        Private, Public, Default, Protected, Static, Final
+    }
+
+    private final CtClass clazz;
+    private final Class<? extends T> genClass;
+
+    public TestClassBuilder(String name, Class<? extends T> genClass) {
         ClassPool cPool = ClassPool.getDefault();
-        CtClass clazz = cPool.makeClass(name);
+        this.clazz = cPool.makeClass(name);
+        this.genClass = genClass;
+    }
+
+    public static String newInstance(Class<?> clazz) {
+        return "new " + clazz.getName() + "()";
+    }
+
+    public static <T> TestClassBuilder<T> forJUnit4(String name, Class<? extends T> genClass) {
+        return new TestClassBuilder<T>(name, genClass);
+    }
+
+    public TestClassBuilder<T> withGenerator(String fieldClass, String fieldName, String fieldValue,
+                                             AccessModifier... mods) {
+        // String fieldString = "%s %s %s = %s;";
+        // String modifiers = Arrays.toString(mods).replace('[', ' ').replace(']', ' ').replace(',', ' ').toLowerCase();
+        // String formatted = String.format(fieldString, modifiers, genClass.getName(), fieldName, fieldValue);
+        try {
+            ClassFile file = clazz.getClassFile();
+            ConstPool constPool = file.getConstPool();
+            String genClassDescriptor = Descriptor.of(genClass.getName());
+            FieldInfo field = new FieldInfo(constPool, fieldName, genClassDescriptor);
+            SignatureAttribute sig = new SignatureAttribute(constPool, genClassDescriptor.split(";")[0] + "<"
+                    + Descriptor.of(fieldClass) + ">;");
+            field.setAccessFlags(Modifier.PUBLIC);
+            field.addAttribute(sig);
+            file.addField(field);
+            CtField newField = clazz.getField(fieldName);
+            // hack
+            clazz.removeField(newField);
+            clazz.addField(newField, fieldValue);
+
+            // this doesn't work as high-level API doesn't support generic type parameters
+            // clazz.addField(CtField.make(formatted, clazz));
+        } catch (CannotCompileException e) {
+            throw new RuntimeException(String.format("Cannot add field %s of type %s to class %s", fieldName,
+                                                     fieldClass, clazz), e);
+        } catch (NotFoundException e) {
+            throw new RuntimeException(String.format("Cannot add field %s of type %s to class %s", fieldName,
+                                                     fieldClass, clazz), e);
+        }
+        return this;
+    }
+
+    public void build() {
         ClassFile file = clazz.getClassFile();
         ConstPool constPool = file.getConstPool();
-
         AnnotationsAttribute attr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
         Annotation runWith = new Annotation(RunWith.class.getName(), constPool);
         runWith.addMemberValue("value", new ClassMemberValue(QuickCheckRunner.class.getName(), constPool));
@@ -29,11 +94,7 @@ public class TestClassBuilder {
         try {
             clazz.toClass();
         } catch (CannotCompileException e) {
-            throw new RuntimeException("Could not create class " + name, e);
+            throw new RuntimeException("Could not create class " + clazz, e);
         }
-    }
-
-    public static TestClassBuilder forJUnit4() {
-        return new TestClassBuilder();
     }
 }
--- jquickcheck-test/src/test/java/lt/dm3/jquickcheck/test/TestClassBuilderTest.java
+++ jquickcheck-test/src/test/java/lt/dm3/jquickcheck/test/TestClassBuilderTest.java
@@ -1,9 +1,18 @@
 package lt.dm3.jquickcheck.test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.typeCompatibleWith;
 import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 import javassist.NotFoundException;
 import lt.dm3.jquickcheck.junit4.QuickCheckRunner;
+import lt.dm3.jquickcheck.sample.Generator;
+import lt.dm3.jquickcheck.sample.IntegerGenerator;
+import lt.dm3.jquickcheck.test.TestClassBuilder.AccessModifier;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,13 +21,34 @@ import org.junit.runner.Runner;
 public class TestClassBuilderTest {
 
     @Test
-    public void shouldCreateATestClass() throws ClassNotFoundException, NotFoundException {
-        TestClassBuilder.forJUnit4().build("lol");
+    public void shouldCreateAJunitTestClassWithAnnotation() throws ClassNotFoundException, NotFoundException {
+        TestClassBuilder.forJUnit4("lol", Generator.class).build();
 
         Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass("lol");
         RunWith runAnnotation = clazz.getAnnotation(RunWith.class);
         Class<? extends Runner> runnerClass = runAnnotation.value();
 
         assertThat(runnerClass, typeCompatibleWith(QuickCheckRunner.class));
+    }
+
+    @Test
+    public void shouldCreateAJunitTestClassWithOneGeneratorOfTheSpecifiedType() throws ClassNotFoundException,
+                                                                               SecurityException, NoSuchFieldException,
+                                                                               InstantiationException,
+                                                                               IllegalAccessException {
+        TestClassBuilder
+                .forJUnit4("lol2", Generator.class)
+                .withGenerator(Integer.class.getName(), "intGen",
+                               TestClassBuilder.newInstance(IntegerGenerator.class),
+                               AccessModifier.Public)
+                .build();
+
+        Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass("lol2");
+        Object instance = clazz.newInstance();
+
+        ParameterizedType pType = (ParameterizedType) clazz.getField("intGen").getGenericType();
+        assertThat(clazz.getField("intGen").getType(), equalTo((Type) Generator.class));
+        assertThat(pType.getActualTypeArguments()[0], equalTo((Type) Integer.class));
+        assertThat(clazz.getField("intGen").get(instance), instanceOf(IntegerGenerator.class));
     }
 }
